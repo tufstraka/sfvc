f3c4bdf609e37d2f469fa4b366929f75fdbf326f
kadimakeith <keithkadima@gmail.com>
2023-04-18 18:56:26 +0300

    fix; define the schema wit more details.

--- model/sales.js
+++ model/sales.js
@@ -1,14 +1,45 @@
 import mongoose from 'mongoose';
 
 const salesData = new mongoose.Schema({
-  name: String,
-  email: String,
-  tshirtSize: String,
-  tshirtType: String,
-  pickup: String,
-  phoneNumber: String,
-  referenceNumber: String,
-  totalAmount: Number,
+  name: {
+          type: String,
+          required: true,
+        },
+  
+  email: {
+          type: String,
+          required: true,
+        },
+  
+  tshirtSize: {
+          type: String,
+          required: true,
+        },
+  
+  tshirtType: {
+          type: String,
+          required: true,
+        },
+  
+  pickup: {
+          type: String,
+          required: true,
+        },
+  
+  phoneNumber: {
+          type: String,
+          required: true,
+        },
+  
+  reference: {
+          type: String,
+          required: true,
+        },
+  
+  totalAmount: {
+          type: Number,
+          required: true,
+        },
 });
 
 const Sale = mongoose.model('Sales', salesData, 'tshirtSales');
